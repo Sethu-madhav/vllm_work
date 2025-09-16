f0179f8675408cd55b90670302f445741dd8699f
Sethu-madhav <thebatman2090@gmail.com>
2025-09-16 17:07:32 +0000

    update Qwen3-4B model to new configuration and enhance sampling parameters

--- qwen3-4B.py
+++ qwen3-4B.py
@@ -2,11 +2,13 @@ from vllm import LLM, SamplingParams
 
 # 1. initialize vllm engine
 llm = LLM(
-          model="Qwen/Qwen3-4B-Instruct-2507", 
+          model="Qwen/Qwen3-4B-Thinking-2507-FP8", 
           tensor_parallel_size=1, 
-          max_model_len=2048,
           max_seq_len_to_capture=2048,
           gpu_memory_utilization=0.9,
+          max_model_len=2048,
+          dtype="auto",
+          quantization="fp8",
         )
 
 # 2. set sampling params
@@ -14,14 +16,14 @@ sampling_params = SamplingParams(
                    temperature=0.7,
                    top_p=0.95,
                    top_k=40,
-                   max_tokens=128,
+                   max_tokens=1024,
                    presence_penalty=0.0,
                    repetition_penalty=1.0,
-                   stop=["\n", "#"]
+                   stop=["#"]
                 )
 # 3. set prompts
 prompts = [
-    "what is the captial of france ?",
+    "what is the capital of france ?",
     "Explain transformer architecture in simple way",
     "What is 30 + 21"
 ]
@@ -37,5 +39,5 @@ for i, output in enumerate(outputs):
     print(output.outputs[0].stop_reason)
     print("-"*40)
 
-# 5. release model weights 
+# 5. remove model weights from gpu
 llm.llm_engine.engine_core.shutdown()
